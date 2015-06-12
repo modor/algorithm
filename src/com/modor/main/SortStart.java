450eeda75c15b35322f904cbb381460cf8271045
modor <wangcf14608@WINDOWS-0L36UH7.mshome.net>
2015-06-12 20:45:11 +0800

    quick sort

--- src/com/modor/main/SortStart.java
+++ src/com/modor/main/SortStart.java
@@ -2,14 +2,14 @@ package com.modor.main;
 
 import com.modor.sort.*;
 
-public class Start {
+public class SortStart {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] testArray = { 10, 23, 4, 1, 44, 45, 22, 67, 76, 82, 12, 34, 45 };
 		int i;
-		SelectSort mySort = new SelectSort();
-		mySort.selectSort(testArray);
+		QuickSort mySort = new QuickSort();
+		mySort.quickSort(testArray, 0, testArray.length - 1);
 		for (i = 0; i < testArray.length; i++) {
 			System.out.println(testArray[i]);
 		}
--- src/com/modor/sort/QuickSort.java
+++ src/com/modor/sort/QuickSort.java
@@ -1,10 +1,36 @@
 package com.modor.sort;
 
 public class QuickSort {
+	public void quickSort(int[] numArray, int arrayLeftBoder,
+			int arrayRightBorder) {
 
-	public static void quickSort(int[] numArray) {
-		int arrayLength = numArray.length;
-		int varLeft = 0;
-		int varRight = arrayLength;
+		if (arrayLeftBoder > arrayRightBorder) {
+			return;
+		}
+		int baseNum = numArray[arrayLeftBoder];
+		int exchange;
+		int leftBoder = arrayLeftBoder;
+		int rightBorder = arrayRightBorder;
+		// System.out.println(arrayLeftBoder);
+
+		while (leftBoder != rightBorder) {
+			while (numArray[rightBorder] >= baseNum && leftBoder < rightBorder) {
+				rightBorder--;
+			}
+			while (numArray[leftBoder] <= baseNum && leftBoder < rightBorder) {
+				leftBoder++;
+			}
+			if (leftBoder < rightBorder) {
+				exchange = numArray[leftBoder];
+				numArray[leftBoder] = numArray[rightBorder];
+				numArray[rightBorder] = exchange;
+			}
+		}
+		numArray[arrayLeftBoder] = numArray[leftBoder];
+		numArray[leftBoder] = baseNum;
+		// System.out.println(leftBoder);
+
+		quickSort(numArray, arrayLeftBoder, leftBoder - 1);
+		quickSort(numArray, leftBoder + 1, arrayRightBorder);
 	}
 }
