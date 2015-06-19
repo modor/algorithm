6c7f31c2ff5bff66c7df9d9ba3e872eb74da1078
modor <wangcf14608@WINDOWS-0L36UH7.hs.handsome.com.cn>
2015-06-19 14:39:39 +0800

    merge sort

--- src/com/modor/main/SortStart.java
+++ src/com/modor/main/SortStart.java
@@ -6,14 +6,15 @@ public class SortStart {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] testArray = { 10, 23, 4, 1, 44, 45, 22, 67, 76, 82, 12, 34, 45, 32,98,65 };
+		int[] testArray = { 10, 23, 4, 1, 44, 45, 22, 67, 76, 82, 12, 34, 45,
+				32, 98, 65 };
 		int i = 0;
-//		Integer in = new Integer(i);
-		InsertionSort mySort = new InsertionSort();
-		mySort.insertionSort(testArray);
-//		System.out.println(in.getClass().getName());
+		// Integer in = new Integer(i);
+		MergeSort mySort = new MergeSort();
+		mySort.mergeSort(testArray, 0, testArray.length - 1);
+		// System.out.println(in.getClass().getName());
 		for (i = 0; i < testArray.length; i++) {
-		System.out.println(testArray[i]);
+			System.out.println(testArray[i]);
 		}
 	}
 }
---
+++ src/com/modor/sort/MergeSort.java
@@ -0,0 +1,40 @@
+package com.modor.sort;
+
+public class MergeSort {
+	public void mergeSort(int[] numArray, int arrayLeftBoder,
+			int arrayRightBorder) {
+		if (arrayLeftBoder >= arrayRightBorder) {
+			return;
+		}
+		int divideNum = (arrayLeftBoder + arrayRightBorder) / 2;
+		mergeSort(numArray, arrayLeftBoder, divideNum);
+		mergeSort(numArray, divideNum + 1, arrayRightBorder);
+
+		merge(numArray, arrayLeftBoder, divideNum, arrayRightBorder);
+	}
+
+	public void merge(int[] numArray, int arrayLeftBoder, int divideNum,
+			int arrayRightBorder) {
+		int[] temArray = new int[numArray.length];
+		int midNum = divideNum + 1;
+		int tmpNum = arrayLeftBoder;
+		int tmpArraynum = arrayLeftBoder;
+
+		while (arrayLeftBoder <= divideNum && midNum <= arrayRightBorder) {
+			if (numArray[arrayLeftBoder] <= numArray[midNum]) {
+				temArray[tmpArraynum++] = numArray[arrayLeftBoder++];
+			} else {
+				temArray[tmpArraynum++] = numArray[midNum++];
+			}
+		}
+		while (midNum <= arrayRightBorder) {
+			temArray[tmpArraynum++] = numArray[midNum++];
+		}
+		while (arrayLeftBoder <= divideNum) {
+			temArray[tmpArraynum++] = numArray[arrayLeftBoder++];
+		}
+		while (tmpNum <= arrayRightBorder) {
+			numArray[tmpNum] = temArray[tmpNum++];
+		}
+	}
+}
